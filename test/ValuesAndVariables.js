2c01c86b753426d1df41edf1932903e9a83171d6
Seung Joo <psjoo3515@gmail.com>
2024-11-15 21:19:11 +0900

    ValuesAndVariables

---
+++ test/ValuesAndVariables.js
@@ -0,0 +1,16 @@
+// 1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+// 2. Log their values to the console.
+
+let country = "Portugal";
+let comtomemt = "Europe";
+let population = 10;
+
+console.log(country);
+console.log(comtomemt);
+console.log(population);
+
+// 결과
+// C:test>node ValuesAndVariables.js
+// Portugal
+// Europe
+// 10
