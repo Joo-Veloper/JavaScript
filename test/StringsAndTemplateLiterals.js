9a6ea7596c3fbc5fe4b8ec47c9a069e21d80c5ba
Seung Joo <psjoo3515@gmail.com>
2024-11-15 22:07:51 +0900

    StringsAndTemplate

---
+++ test/StringsAndTemplateLiterals.js
@@ -0,0 +1,8 @@
+// Recreate the description variable from the last assignment, this time using the template literal syntax.
+const country = "SEOUL";
+const continent = "ASIA";
+let population = 10;
+let language = "KOREAN";
+
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description);
