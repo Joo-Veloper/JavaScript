a5bda1d66112b70bc0d2a7f2a64b681114df62b2
Seung Joo <psjoo3515@gmail.com>
2024-11-21 21:31:03 +0900

    Data Type

--- part1/script.js
+++ part1/script.js
@@ -40,3 +40,25 @@ let job1 = "programmer";
 let job2 = " teacher";
 
 console.log(myFirstJob);
+
+// DATA TYPE
+true;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 24);
+console.log(typeof "joo");
+
+javascriptIsFun = "YES";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
