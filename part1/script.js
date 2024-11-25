673e5d7a0092a91e85f64798e61bea34bfe0816c
Seung Joo <psjoo3515@gmail.com>
2024-11-25 20:17:36 +0900

    operators

--- part1/script.js
+++ part1/script.js
@@ -72,3 +72,31 @@ var job = "programmer";
 job = "teacher";
 lastName = "Schmedtmann";
 console.log(lastName);
+
+// Operators
+const now = 2037;
+const ageJonas = now - 1990;
+const ageSara = now - 2018;
+console.log(ageJonas, ageSara);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2**3 means 2 the power of 3=2*2*2
+const firstName1 = "Java";
+const lastName1 = "Script";
+console.log(firstName1 + " " + lastName1);
+
+//Assignment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+//Comparions operators
+console.log(ageJonas > ageSara);
+console.log(ageSara >= 19);
+
+const isFullAge = ageSara >= 18;
+console.log(now - 1991 > now - 2018);
